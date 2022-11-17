fcd1f38641b426ce12307231f4bc9f44148e4158
satriyarp <rifkisatriya@gmail.com>
2022-11-17 23:07:49 +0700

    next one

--- Aritmatika.java
+++ Aritmatika.java
@@ -4,10 +4,11 @@ public class Aritmatika {
     public static void main(String[] args) {
         //System.out.println("Hello docker. :)");
 	Scanner sc = new Scanner(System.in);
+	Scanner su = new Scanner(System.in);
 	System.out.print("Input nilai: ");
 	double val = sc.nextDouble();
 	System.out.print("Input nilai: ");
-	double val1 = sc.nextDouble();
+	double val1 = su.nextDouble();
 	System.out.print("Input paramter please(*/+-): ");
 	char op = sc.next().charAt(0);
 	operator(op,val,val1);
--- main.java
+++
@@ -1,5 +0,0 @@
-public class helo(){
-    public static void main(String[] args) {
-        System.println("Halo Gaes")
-    }
-}
